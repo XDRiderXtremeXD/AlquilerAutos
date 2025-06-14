d1cf4f0740941e2eff959b16c94db37f29f940de
Angelo <rideralucar@gmail.com>
2025-06-14 10:18:15 -0500

    Se modifico el layout, se realizo logica de alquiler (Controller, vista,interface, falta el formulario

---
+++ src/main/java/proyectosCibertec/com/model/Alquiler.java
@@ -0,0 +1,35 @@
+package proyectosCibertec.com.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="alquiler")
+public class Alquiler {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer idCliente;
+    private Integer idVehiculo;
+    private Integer numDias;
+    private BigDecimal precioDia;
+    private BigDecimal abono;
+    private LocalDate fechaPrestamo;
+    private LocalTime hora;
+    private LocalDate fechaDevolucion;
+    private Integer idDoc;
+    private String observacion;
+    private Integer estado;
+
+    // Getters y setters
+}
