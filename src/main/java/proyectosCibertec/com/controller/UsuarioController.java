d047d87094b52c28796a7201ea5f50012e249ef1
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2025-06-17 00:30:35 -0500

    agregar funcionalidad a editar

--- src/main/java/proyectosCibertec/com/controller/UsuarioController.java
+++ src/main/java/proyectosCibertec/com/controller/UsuarioController.java
@@ -15,31 +15,22 @@ import proyectosCibertec.com.repository.IUsuarioRepository;
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private IUsuarioRepository repoUsu;
-	
-	// Muestra la lista de usuarios
+
 	@GetMapping("/listado")
 	public String listarUsuarios(Model model) {
-		
+
 		model.addAttribute("lstUsuarios", repoUsu.findAll());
 		model.addAttribute("usuario", new Usuario());
-		return "usuario/listadoUsuario";
+		return "usuario/usuarios";
 	}
-	
-	@GetMapping("/registrar")
-	public String mostrarRistrar(Model model) {
-		
-		model.addAttribute("usuario", new Usuario());
-		return "usuario/registroUsuario";
-	}
-	
+
 	@PostMapping("/grabar")
 
 	public String registrarUsuario(@ModelAttribute Usuario usuario, Model model) {
 
-		
 		try {
 			repoUsu.save(usuario);
 			model.addAttribute("mensaje", "Usuario registrado exitosamente");
@@ -48,40 +39,38 @@ public class UsuarioController {
 			model.addAttribute("mensaje", "Error al registrar" + e.getMessage());
 			model.addAttribute("cssmensaje", "alert alert-danger");
 		}
-		
+
 		return "redirect:/usuarios/listado";
 
 	}
-	
-	
+
 	@GetMapping("/editar/{id}")
 	public String mostrarEdicion(@PathVariable int id, Model model) {
-		
-		System.out.println("usuario --->  "+ id);
-		
 
-	    Usuario usuario = repoUsu.findById(id).orElse(null);
-		
-	    if (usuario != null) {
-	        model.addAttribute("usuario", usuario);
-	    } else {
-	        model.addAttribute("mensaje", "Usuario no encontrado");
-	        model.addAttribute("cssmensaje", "alert alert-danger");
-	    }
-	    return "usuario/editarUsuario";
+		Usuario usuario = repoUsu.findById(id).get();
+		model.addAttribute("usuario", usuario);
+
+		return "usuario/editarUsuario";
 	}
 
-    @PostMapping("/actualizar")
-    public String actualizar(@ModelAttribute Usuario usuario, Model model) {
-        try {
-            repoUsu.save(usuario);
-            model.addAttribute("mensaje", "Usuario actualizado exitosamente");
-            model.addAttribute("cssmensaje", "alert alert-success");
-        } catch (Exception e) {
-            model.addAttribute("mensaje", "Error al actualizar: ".concat(e.getMessage()));
-            model.addAttribute("cssmensaje", "alert alert-danger");
-        }
-        return "redirect:/usuarios/listado";
-    }
-	
+	@PostMapping("/actualizar")
+	public String actualizar(@ModelAttribute Usuario usuario, Model model) {
+		try {
+			repoUsu.save(usuario);
+			model.addAttribute("mensaje", "Usuario actualizado exitosamente");
+			model.addAttribute("cssmensaje", "alert alert-success");
+		} catch (Exception e) {
+			model.addAttribute("mensaje", "Error al actualizar: ".concat(e.getMessage()));
+			model.addAttribute("cssmensaje", "alert alert-danger");
+		}
+		return "redirect:/usuarios/listado";
+	}
+
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUsuario(@PathVariable Integer id) {
+
+		repoUsu.deleteById(id);
+		return "redirect:/usuarios/listado";
+	}
+
 }
