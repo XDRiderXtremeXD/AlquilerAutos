fcbdb3bc2cedc8ebe8cef184e10950c9e2b01745
Juan <juanjoseblas344@gmail.com>
2025-06-21 05:31:32 -0500

    Se agregó en el formulario de Documentos y Vehiculos un input para fotos, el cual agrega la foto con el id del registro, se guarda en una carpeta (se crea en caso no exista) y al Eliminar se borra de la carpeta y de la base de datos. Se agregó una columna 'fotos' en la tabla documentos. Agregar foto también tiene restricciones, solo jpg/jpeg y menos a 1mb, validaciónes en el front (respuesta inmediata) y back (por mayor seguridad)

--- src/main/java/proyectosCibertec/com/controller/VehiculoController.java
+++ src/main/java/proyectosCibertec/com/controller/VehiculoController.java
@@ -1,6 +1,10 @@
 package proyectosCibertec.com.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import proyectosCibertec.com.model.Marcas;
@@ -61,6 +67,18 @@ public class VehiculoController {
 	@GetMapping("/eliminar/{id}")
 	public String eliminarVehiculo(@PathVariable int id, RedirectAttributes redirAtributos) {
 		try {
+			Vehiculos vehiculo = repoVehiculo.findById(id).orElse(null);
+			
+			// Borrar imagen de la carpeta
+	        if (vehiculo != null) {
+	            String nombreFoto = vehiculo.getFoto();
+	            
+	            if (nombreFoto != null && !nombreFoto.isBlank()) {
+	                Path rutaFoto = Paths.get("uploads/img/vehiculos/", nombreFoto);
+	                Files.deleteIfExists(rutaFoto);
+	            }
+	        }
+			
 			repoVehiculo.deleteById(id);
 			redirAtributos.addFlashAttribute("mensaje", "Vehículo eliminado correctamente");
             redirAtributos.addFlashAttribute("css_mensaje", "alert alert-success");
@@ -73,10 +91,47 @@ public class VehiculoController {
 	}
     
 	    @PostMapping("/registro")
-	    public String registrarVehiculo(@ModelAttribute Vehiculos vehiculos, 
+	    public String registrarVehiculo(@ModelAttribute Vehiculos vehiculos,
+	    		@RequestParam("fileFoto") MultipartFile file,
 	    		RedirectAttributes redirAtributos) {
 	        try {
-	            repoVehiculo.save(vehiculos);
+		        // Validación de archivo vacío
+		        if (!file.isEmpty()) {
+		        		
+		            // Validar que sea JPG o JPEG
+		            String extension = file.getOriginalFilename().toLowerCase();
+		            if (!extension.endsWith(".jpg") && !extension.endsWith(".jpeg")) {
+		                redirAtributos.addFlashAttribute("mensaje", "La imagen debe estar en formato JPG o JPEG");
+		                redirAtributos.addFlashAttribute("css_mensaje", "alert alert-danger");
+		                return "redirect:/vehiculos/listado";
+		            }
+
+		            // Validación de tamaño máximo 1MB
+		            if (file.getSize() > 1048576) {
+		                redirAtributos.addFlashAttribute("mensaje", "La imagen no puede superar 1MB");
+		                redirAtributos.addFlashAttribute("css_mensaje", "alert alert-danger");
+		                return "redirect:/vehiculos/listado";
+		            }
+		        }
+		        
+	        	// Guardar Vehiculo para obtener el ID
+	            Vehiculos vehiculoGuardado = repoVehiculo.save(vehiculos);
+	            int idGenerado = vehiculoGuardado.getId();
+	            
+	            // Carpeta de imágenes
+	        	String rutaCarpeta = "uploads/img/vehiculos/";
+	            Files.createDirectories(Paths.get(rutaCarpeta)); // Se crea en caso no exista
+	            
+	            // Guardar imagen con nombre ID.jpg
+	            String nombreArchivo = idGenerado + ".jpg";
+	            Path rutaCompleta = Paths.get(rutaCarpeta + nombreArchivo);
+	            Files.createDirectories(rutaCompleta.getParent()); // Crear carpeta si no existe
+	            Files.write(rutaCompleta, file.getBytes());
+	            
+	            // Guardar en la BD
+	            vehiculoGuardado.setFoto(nombreArchivo);
+	            repoVehiculo.save(vehiculoGuardado);
+	            
 	            redirAtributos.addFlashAttribute("mensaje", "Vehículo registrado correctamente");
 	            redirAtributos.addFlashAttribute("css_mensaje", "alert alert-success");
 	        } catch (Exception e) {
