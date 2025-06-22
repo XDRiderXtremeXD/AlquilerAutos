45d59aa115fb8fc97b87f4105f68b7470b9e9391
Juan <juanjoseblas344@gmail.com>
2025-06-21 23:58:17 -0500

    Se agrego la funcionalidad Editar en Vehiculos

--- src/main/resources/static/js/scrips.js
+++ src/main/resources/static/js/scrips.js
@@ -1,12 +1,27 @@
 function llenarModalEditar(btn) {
-			const datos = btn.dataset; // obtiene todos los atributos data-*
-			console.log(datos);
-			//Hace un for de todos los atributos mandados y modifica los elementos de editar de algun formulario de editar con el nombre "editar"+ATRIBUTO				
-				for (let key in datos) {
-					if (key.startsWith("bs")) continue; // ignorar atributos Bootstrap como bsToggle, bsTarget
-				    const input = document.getElementById("editar" + key.charAt(0).toUpperCase() + key.slice(1));
-				    if (input) {
-				        input.value = btn.getAttribute("data-" + key);
-				    }
-  			}
+	const datos = btn.dataset; // obtiene todos los atributos data-*
+	console.log(datos);
+	//Hace un for de todos los atributos mandados y modifica los elementos de editar de algun formulario de editar con el nombre "editar"+ATRIBUTO				
+	for (let key in datos) {
+		if (key.startsWith("bs")) continue; // ignorar atributos Bootstrap como bsToggle, bsTarget
+		const input = document.getElementById("editar" + key.charAt(0).toUpperCase() + key.slice(1));
+		if (input) {
+			input.value = btn.getAttribute("data-" + key);
 		}
+	}
+
+
+}
+
+function borrarImagen() {
+	inputFoto.value = '';
+	minFoto.src = '';
+	contFoto.style.display = 'none';
+}
+
+function borrarImagenEdit() {
+	inputFotoEdit.value = '';
+	minFotoEdit.src = '';
+	contFotoEdit.style.display = 'none';
+	document.getElementById('alertaFotoEdit').classList.add('d-none');
+}
