94a4be1d92b93a360e51aaf452ea6a814f573092
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2025-06-29 17:46:00 -0500

    me modifico la vista de contacto

--- src/main/java/proyectosCibertec/com/controller/ClientesPageController.java
+++ src/main/java/proyectosCibertec/com/controller/ClientesPageController.java
@@ -1,13 +1,23 @@
 package proyectosCibertec.com.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import proyectosCibertec.com.model.Configuracion;
+import proyectosCibertec.com.repository.IConfiguracionRepository;
 
 @Controller
 public class ClientesPageController {
 
+	@Autowired
+    private IConfiguracionRepository repoConfig;
+	
 	@GetMapping("/contactos")
-    public String contactosVista() {
+    public String contactosVista(Model model) {
+		Configuracion configuracion = repoConfig.findById(1).orElseThrow();
+	    model.addAttribute("configuracion", configuracion);
         return "contentclient/contactos";
     }
 	
