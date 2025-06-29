26cc616c174492f825e0c1ac61fd207a1845111f
Jhuli Trujillo <jhulitrujillomeza@gmail.com>
2025-06-28 23:36:18 -0500

    modificaciones de la vista de usuario

--- src/main/java/proyectosCibertec/com/controller/UsuarioController.java
+++ src/main/java/proyectosCibertec/com/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import proyectosCibertec.com.model.Usuario;
 import proyectosCibertec.com.repository.IUsuarioRepository;
@@ -19,24 +20,23 @@ import proyectosCibertec.com.repository.IUsuarioRepository;
 @Controller
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
 	@Autowired
 	private IUsuarioRepository repoUsu;
-	
+
 	@GetMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
+	public String loginPage() {
+		return "login";
+	}
 
 	@GetMapping("/listado")
-	public String usuarioCrud(Model model,  @RequestParam(defaultValue = "0") int page,
+	public String usuarioCrud(Model model, @RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "5") int size) {
 
-		Page<Usuario >lstUsuarios = repoUsu.findByEstado(1, PageRequest.of(page, size));
-		model.addAttribute("lstUsuarios",lstUsuarios);
+		Page<Usuario> lstUsuarios = repoUsu.findByEstado(1, PageRequest.of(page, size));
+		model.addAttribute("lstUsuarios", lstUsuarios);
 		model.addAttribute("paginaActual", page);
 		model.addAttribute("tamanio", size);
 		model.addAttribute("usuario", new Usuario());
@@ -44,39 +44,62 @@ public class UsuarioController {
 	}
 
 	@PostMapping("/grabar")
-	public String registrarUsuario(@ModelAttribute Usuario usuario, Model model) {
-		
+	public String registrarUsuario(@ModelAttribute Usuario usuario,
+			RedirectAttributes redirAtributos) {
+
 		try {
 			usuario.setClave(passwordEncoder.encode(usuario.getClave()));
 			repoUsu.save(usuario);
-			model.addAttribute("mensaje", "Usuario registrado exitosamente");
-			model.addAttribute("cssmensaje", "alert alert-success");
+
+			redirAtributos.addFlashAttribute("mensaje", "Usuario registrado exitosamente");
+			redirAtributos.addFlashAttribute("cssmensaje", "alert alert-success");
 		} catch (Exception e) {
-			model.addAttribute("mensaje", "Error al registrar" + e.getMessage());
-			model.addAttribute("cssmensaje", "alert alert-danger");
+			redirAtributos.addFlashAttribute("mensaje", "Error al registrar" + e.getMessage());
+			redirAtributos.addFlashAttribute("cssmensaje", "alert alert-danger");
 		}
 
 		return "redirect:/usuarios/listado";
 
 	}
 
+	@PostMapping("/actualizar")
+	public String actualizarUsuario(@ModelAttribute Usuario usuario, RedirectAttributes redirAtributos) {
+
+		try {			
+			usuario.setClave(passwordEncoder.encode(usuario.getClave()));
+			repoUsu.save(usuario);
+
+			redirAtributos.addFlashAttribute("mensaje", "Usuario actualizado exitosamente");
+			redirAtributos.addFlashAttribute("cssmensaje", "alert alert-success");
+		} catch (Exception e) {
+			redirAtributos.addFlashAttribute("mensaje", "Error al registrar" + e.getMessage());
+			redirAtributos.addFlashAttribute("cssmensaje", "alert alert-danger");
+		}
+
+		return "redirect:/usuarios/listado";
+
+	}
 
 	@GetMapping("/eliminar/{id}")
-	public String eliminarUsuario(@PathVariable Integer id, Model model) {
-		
-		Usuario usuario = repoUsu.findById(id).get();
-		
+	public String eliminarUsuario(@PathVariable Integer id, RedirectAttributes addFlashAttribute) {
+
 		try {
-			usuario.setEstado(0);
-			repoUsu.save(usuario);
-			model.addAttribute("mensaje", "Usuario eliminado exitosamente");
-			model.addAttribute("cssmensaje", "alert alert-success");
+
+			Usuario usuario = repoUsu.findById(id).orElse(null);
+			if (usuario != null) {
+				usuario.setEstado(0);
+				repoUsu.save(usuario);
+				addFlashAttribute.addAttribute("mensaje", "Usuario eliminado exitosamente");
+				addFlashAttribute.addAttribute("cssmensaje", "alert alert-success");
+			}
+
 		} catch (Exception e) {
-			model.addAttribute("mensaje", "Error al registrar" + e.getMessage());
-			model.addAttribute("cssmensaje", "alert alert-danger");
+			addFlashAttribute.addAttribute("mensaje", "Error al registrar" + e.getMessage());
+			addFlashAttribute.addAttribute("cssmensaje", "alert alert-danger");
 		}
 
 		return "redirect:/usuarios/listado";
 	}
+
 
 }
